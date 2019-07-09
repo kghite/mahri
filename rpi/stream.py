5cfaede1ede6b7cc03f403334eb274ac8d6a084a
kghite <kghite@gmail.com>
2019-07-09 16:40:37 -0400

    MOVE: Todo to Trello

--- rpi/stream.py
+++ rpi/stream.py
@@ -2,3 +2,21 @@
 Read from server stream to manage robot, send sensor stream back
 Manage robot state in reference to interaction tempo
 """
+
+class Stream:
+
+	def __init__(self):
+		self.tempo = None
+		self.server = None
+
+	def set_tempo(self, bpm):
+		pass
+
+	def get_update(self):
+		pass
+
+	def set_state(self, state):
+		pass
+
+	def send_update(self):
+		pass
